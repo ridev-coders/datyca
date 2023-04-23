aa4287e883e73543237122a3aa65873ffb6f01e4
riscaldo <riccardo85ferrara@gmail.com>
2023-04-23 22:55:07 +0200

    fix menu line widht behaviour

--- menu.js
+++ menu.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
         button.addEventListener('click', () => {
             const target = button.getAttribute('data-target');
             const targetSection = document.querySelector('.menu-section.' + target);
+            const parent = button.parentElement
 
             document.querySelectorAll('.menu-section').forEach(section => {
                 section.style.display = 'none';
@@ -11,9 +12,13 @@ document.addEventListener('DOMContentLoaded', () => {
             document.querySelectorAll('.side-menu .button').forEach(button => {
                 button.classList.remove('active')
             });
+            document.querySelectorAll('.side-menu .element').forEach(button => {
+                button.classList.remove('active')
+            });
 
             targetSection.style.display = 'block';
             button.classList.add('active')
+            parent.classList.add('active')
         });
     });
 });
