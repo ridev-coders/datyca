b8187ad9998bdd8d83c64b989eab59fffb88446c
riscaldo <riccardo85ferrara@gmail.com>
2023-04-28 19:24:09 +0200

    add navbar transition when scrolling

--- navbar.js
+++ navbar.js
@@ -1,24 +1,40 @@
 window.onload = function() {
     const anchors = document.querySelectorAll('a[href^="#"]');
     const navLinks = [...document.querySelectorAll('nav ul li a')];
-    
+    const navbar = document.querySelector('header');
+
     function onScroll() {
-      const scrollPosition = window.scrollY;
-      anchors.forEach(anchor => {
+
+        // change style for anchors links
+        const scrollPosition = window.scrollY;
+        anchors.forEach(anchor => {
         const target = document.querySelector(anchor.hash);
         if (target.offsetTop <= scrollPosition && target.offsetTop + target.offsetHeight > scrollPosition) {
-          navLinks.forEach(link => {
-            // reset all links color first
-            link.style.color = '';
-            link.classList.remove('selected')
-          });
-    
-          // set the color to blue for the active link
-          anchor.classList.add('selected')
+            navLinks.forEach(link => {
+                // reset all links color first
+                link.style.color = '';
+                link.classList.remove('selected')
+            });
+
+            // set the color to blue for the active link
+            anchor.classList.add('selected')
         }
-      });
+        });
+
+        // change style for navbar
+        if (window.pageYOffset === 0) {
+            // We're at the top of the page
+            navbar.classList.remove('invisible');
+            navbar.classList.remove('blue-background');
+            navbar.classList.add('visible');
+        } else {
+            // We're scrolling down
+            navbar.classList.add('invisible');
+            navbar.classList.add('blue-background');
+            navbar.classList.remove('visible');
+        }
     }
-    
+
     window.addEventListener('scroll', onScroll);
 };
   
