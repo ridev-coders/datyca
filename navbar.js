b6356090142b1558c77941b0cb85cede530dab35
riscaldo <riccardo85ferrara@gmail.com>
2023-05-01 19:06:44 +0200

    fix settori page bg

--- navbar.js
+++ navbar.js
@@ -2,56 +2,63 @@ window.onload = function() {
     const anchors = document.querySelectorAll('a[href^="#"]');
     const navLinks = [...document.querySelectorAll('nav ul li a')];
     const navbar = document.querySelector('header');
+    const isHomepage = document.querySelector('.page.hp');
 
     function onScroll() {
 
         // change style for anchors links
-        const scrollPosition = window.scrollY;
-        const windowHeight = window.innerHeight;
-        const docHeight = document.documentElement.scrollHeight;
-        anchors.forEach(anchor => {
-        const target = document.querySelector(anchor.hash);
-          // Determine if the user has reached the bottom of the page
-        if (scrollPosition + windowHeight >= docHeight) {
-            navLinks.forEach(link => {
-                // reset all links color first
-                link.style.color = '';
-                link.classList.remove('selected')
+        function linksStyle() {
+            const scrollPosition = window.scrollY;
+            const windowHeight = window.innerHeight;
+            const docHeight = document.documentElement.scrollHeight;
+            anchors.forEach(anchor => {
+                const target = document.querySelector(anchor.hash);
+        
+                // Determine if the user has reached the bottom of the page
+                if (scrollPosition + windowHeight >= docHeight) {
+                    navLinks.forEach(link => {
+                        // reset all links color first
+                        link.style.color = '';
+                        link.classList.remove('selected')
+                    });
+                    // Select the last anchor element and add the "selected" class
+                    const lastAnchor = anchors[anchors.length - 1];
+                    lastAnchor.classList.add('selected');
+                } else {
+                    if (target.offsetTop <= scrollPosition +2 && target.offsetTop + target.offsetHeight > scrollPosition) {
+                        navLinks.forEach(link => {
+                            // reset all links color first
+                            link.style.color = '';
+                            link.classList.remove('selected')
+                        });
+            
+                        // set the color to blue for the active link
+                        anchor.classList.add('selected')
+                    }
+                }
             });
-            // Select the last anchor element and add the "selected" class
-            const lastAnchor = anchors[anchors.length - 1];
-            lastAnchor.classList.add('selected');
-        } else {
-            if (target.offsetTop <= scrollPosition +2 && target.offsetTop + target.offsetHeight > scrollPosition) {
-                navLinks.forEach(link => {
-                    // reset all links color first
-                    link.style.color = '';
-                    link.classList.remove('selected')
-                });
-    
-                // set the color to blue for the active link
-                anchor.classList.add('selected')
-            }
         }
-        });
 
         // change style for navbar
-        if (window.pageYOffset === 0) {
-            // We're at the top of the page
-            navbar.classList.remove('invisible');
-            navbar.classList.remove('blue-background');
-            navbar.classList.add('visible');
-        } else {
-            // We're scrolling down
-            navbar.classList.add('invisible');
-            navbar.classList.add('blue-background');
-            navbar.classList.remove('visible');
+        function backgroundStyle() {
+            if (window.pageYOffset === 0) {
+                // We're at the top of the page
+                navbar.classList.remove('invisible');
+                navbar.classList.remove('blue-background');
+                navbar.classList.add('visible');
+            } else {
+                // We're scrolling down
+                navbar.classList.add('invisible');
+                navbar.classList.add('blue-background');
+                navbar.classList.remove('visible');
+            }
         }
+
+        if (isHomepage){
+            linksStyle();
+        }
+        backgroundStyle();
     }
 
     window.addEventListener('scroll', onScroll);
 };
-  
-
-
-
