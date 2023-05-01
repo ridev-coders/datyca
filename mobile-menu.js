883afa4695dbf44367c6e5a1d7be000649e4dd05
riscaldo <riccardo85ferrara@gmail.com>
2023-05-01 22:47:28 +0200

    mobile bg

--- mobile-menu.js
+++ mobile-menu.js
@@ -3,10 +3,13 @@ window.onload = function() {
     // mobile menu
     const hamburger = document.querySelector('.hamburger');
     const navLinks = document.querySelector('ul');
+    const background = document.querySelector('.bg-lines');
 
     hamburger.addEventListener('click', () => {
         navLinks.classList.toggle('hide');
         hamburger.classList.toggle('open')
         hamburger.classList.toggle('close')
+        background.classList.toggle('hide')
+        background.classList.toggle('show')
     });
 }
