47609c748b92de116a279fe01272db38cfd4104f
riscaldo <riccardo85ferrara@gmail.com>
2023-05-22 23:07:23 +0200

    animations do not have to work when page is loaded with anchors

--- hp.js
+++ hp.js
@@ -1,23 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const targetElement1 = document.getElementById('logo-container');
-    const targetElement2 = document.getElementById('big-logo-container');
+    const targetElements = [];
+    // remove animations when scroll is more then 160
+    // or when page is loaded with anchor
+    targetElements.push(document.getElementById('logo-container'));
+    targetElements.push(document.getElementById('big-logo-container'));
+    targetElements.push(document.querySelector('#slogan .content.slogan'));
+    targetElements.push(document.querySelector('div.slogan span.future'));
+    targetElements.push(document.querySelector('.bg-lines.desktop'));
+    targetElements.push(document.querySelector('.bg-lines.mobile'));
+    
+    if (window.pageYOffset < 160 && !window.location.hash) {
+        targetElements.forEach(function(targetElement) {
+            targetElement.classList.add('animate');
+        });
+    } else {
+        targetElements.forEach(function(targetElement) {
+            targetElement.classList.remove('animate');
+        });
+    }
 
-        console.log(window.pageYOffset)
-        if (window.pageYOffset < 160 && !window.location.hash) {
-            targetElement1.classList.add('animate');
-            targetElement2.classList.add('animate');
-        } else {
-            targetElement1.classList.remove('animate');
-            targetElement2.classList.remove('animate');
-        }
-
-        // remove the big logo:
-        // Get a reference to the big logo container element
-        const bigLogoContainer = document.querySelector('#big-logo-container');
-
-        // Wait for 5 seconds using setTimeout() function
-        setTimeout(() => {
-        // After 5 seconds, set the display property of big logo container to none
+    // if user landed in page from anchor, remove the big logo:
+    const bigLogoContainer = document.querySelector('#big-logo-container');
+    if (window.location.hash){
         bigLogoContainer.style.display = 'none';
-        }, 5000); // 5000 milliseconds = 5 seconds
+    }
+
 });
