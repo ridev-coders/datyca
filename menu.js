ecbd4f883235240aeeaed5956aea040fce9c96db
riscaldo <riccardo85ferrara@gmail.com>
2023-06-03 09:53:11 +0200

    cosa facciamo menu has fluid transitions

--- menu.js
+++ menu.js
@@ -1,16 +1,32 @@
-
 document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.menu-section');
+    
+    // Store the height of each section in a data attribute
+    Array.from(sections).forEach(section => {
+        section.dataset.height = section.scrollHeight;
+    });
+
     document.querySelectorAll('.side-menu .button').forEach(button => {
         button.addEventListener('click', () => {
+            const target = button.getAttribute('data-target');
+            const targetSection = document.querySelector('.menu-section.' + target);
             const parent = button.parentElement
 
-            document.querySelectorAll('.side-menu .button').forEach(button => {
-                button.classList.remove('active')
-            });
-            document.querySelectorAll('.side-menu .element').forEach(button => {
-                button.classList.remove('active')
+            // Loop through all sections and set their height to 0
+            Array.from(sections).forEach(section => {
+                section.style.height = 0;
             });
 
+            // Remove the "active" class from all buttons and elements
+            document.querySelectorAll('.side-menu .button, .side-menu .element').forEach(element => {
+                element.classList.remove('active');
+            });
+
+            // Set the height of the target section to its stored height
+            targetSection.style.display = 'block';
+            targetSection.style.height = targetSection.dataset.height + 'px';
+
+            // Add the "active" class to the current button and its parent element
             button.classList.add('active')
             parent.classList.add('active')
         });
