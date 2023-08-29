2db582d0abbc261ca016152873e747fcf9febeeb
riscaldo <riccardo85ferrara@gmail.com>
2023-08-29 19:29:41 +0200

    fix mobile menu background

--- mobile-menu.js
+++ mobile-menu.js
@@ -6,6 +6,8 @@ window.addEventListener('load', function() {
     const background = document.querySelector('.bg-lines');
     const slogan = document.querySelector('.slogan .mobile')
     const navLinks = document.querySelectorAll('.navbar ul li');
+    const header = document.querySelector('header');
+    const navbar = document.querySelector('.navbar');
     // not display menu in mobile
     if (window.matchMedia("(max-width: 480px)").matches) {
         navLinkUl.classList.add('zero-size');
@@ -37,12 +39,18 @@ window.addEventListener('load', function() {
             navLinkUl.classList.toggle('closed');
             navLinkUl.classList.remove('zero-size');
             navLinkUl.classList.toggle('hide');
+            navbar.classList.toggle('full-height');
+            header.classList.toggle('full-height');
+            header.classList.toggle('blue-background');
             // navLinkUl.classList.toggle('show');
         } else {
             // Otherwise add display-none class after 2 seconds
             navLinkUl.classList.toggle('hide');
             navLinkUl.classList.toggle('closed');
             navLinkUl.classList.toggle('opened');
+            header.classList.toggle('blue-background');
+            navbar.classList.toggle('full-height');
+            header.classList.toggle('full-height');
             // navLinkUl.classList.toggle('show');
             setTimeout(() => {
                 navLinkUl.classList.add('zero-size');
